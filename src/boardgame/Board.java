5705cd23993f82ad3cbcc90d100dbbd0285e535c
Thiago <thiagoluis_10@hotmail.com>
2022-12-20 15:33:53 -0300

    small update

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -11,6 +11,24 @@ public class Board {
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
 	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+	
+	
 	
 	
 
